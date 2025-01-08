e049ebe89bc6145e8d53543cbcc1b8fd781bf0cf
rqphy <raphael.ferreira@hetic.net>
2025-01-08 15:13:35 +0100

    :bug: ensure to start a next round when timer goes to 0

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -3,34 +3,34 @@ import { useSocket } from "../../contexts/SocketManager"
 import { useParams } from "react-router-dom"
 
 interface ITimerProps {
-	targetDate: Date
+    targetDate: Date
 }
 
 export default function Timer({ targetDate }: ITimerProps) {
-	const [secondsLeft, setSecondsLeft] = useState<number>(19)
-	const { methods } = useSocket()
-	const { lobbyId } = useParams()
+    const [secondsLeft, setSecondsLeft] = useState<number>(19)
+    const { methods } = useSocket()
+    const { lobbyId } = useParams()
 
-	useEffect(() => {
-		function calculateSecondsLeft() {
-			const now: Date = new Date()
-			const difference = Math.floor(
-				(new Date(targetDate).getTime() - now.getTime()) / 1000
-			)
+    useEffect(() => {
+        function calculateSecondsLeft() {
+            const now: Date = new Date()
+            const difference = Math.floor(
+                (new Date(targetDate).getTime() - now.getTime()) / 1000
+            )
 
-			if (difference === 0) {
-				methods.timerEnded(lobbyId as string)
-			}
+            if (difference <= 0) {
+                methods.timerEnded(lobbyId as string)
+            }
 
-			return difference > 0 ? difference : 0 // ensure it's doesn't go negative
-		}
+            return difference > 0 ? difference : 0 // ensure it's doesn't go negative
+        }
 
-		const interval = setInterval(() => {
-			setSecondsLeft(calculateSecondsLeft())
-		}, 1000)
+        const interval = setInterval(() => {
+            setSecondsLeft(calculateSecondsLeft())
+        }, 1000)
 
-		return () => clearInterval(interval)
-	})
+        return () => clearInterval(interval)
+    })
 
-	return <div>{secondsLeft}s</div>
+    return <div>{secondsLeft}s</div>
 }
