4736662991204de4f4c8b17347509b9c5c30efb0
rqphy <raphael.ferreira@hetic.net>
2025-01-10 18:41:49 +0100

    :sparkles: update header with geoquiz logo

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import "./header.scss"
 import { Link } from "react-router-dom"
 export default function Header() {
-	return (
-		<header>
-			<Link to={"/"}>Home</Link>
-		</header>
-	)
+    return (
+        <header>
+            <Link to={"/"}>
+                <img src="/logo.png" alt="GeoQuiz" />
+            </Link>
+        </header>
+    )
 }
