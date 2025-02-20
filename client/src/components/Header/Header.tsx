af30573d1a9b0a6fba6800ee298a694cdeedfb37
rqphy <raphael.ferreira@hetic.net>
2025-02-20 15:54:26 +0100

    :lipstick: update colors and header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import "./header.scss"
 import { Link } from "react-router-dom"
 export default function Header() {
-    return (
-        <header>
-            <Link to={"/"}>
-                <img src="/logo.png" alt="GeoQuiz" />
-            </Link>
-        </header>
-    )
+	return (
+		<header>
+			<Link to={"/"} id="homelink">
+				GeoQuiz
+			</Link>
+			<a href="https://github.com/rqphy/GeoQuiz">Github</a>
+		</header>
+	)
 }
